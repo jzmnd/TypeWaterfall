34efe01c1e98114a82152f07d0204c6b236533bd
Jez Smith <jzmnd@users.noreply.github.com>
2025-02-02 15:58:11 -0600

    Build using webpack (#9)

---
+++ src/constants.ts
@@ -0,0 +1,39 @@
+// List of the most common font weight names ordered from lowest to highest weight
+export const fontWeightOrder = [
+    'hairline',
+    'thin',
+    'ultra light',
+    'ultralight',
+    'extra light',
+    'extralight',
+    'light',
+    'normal',
+    'regular',
+    'medium',
+    'semi bold',
+    'semibold',
+    'demi bold',
+    'demibold',
+    'bold',
+    'extra bold',
+    'extrabold',
+    'ultra bold',
+    'ultrabold',
+    'black',
+    'heavy',
+    'ultra',
+];
+export const regularFontWeightIdx = 8;
+
+// List of common font widths ordered from widest to narrowest
+export const fontWidthOrder = [
+    'wide',
+    'semiwide',
+    'narrow',
+    'condensed',
+    'compressed',
+];
+export const regularFontWidthIdx = 1.5;
+
+// Default padding amount
+export const defaultPad = 5;
