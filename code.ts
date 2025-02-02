08506013bcf590abe6dc7e3e3e46fd6da5cf48d8
Jez Smith <jzmnd@users.noreply.github.com>
2025-02-01 19:35:10 -0600

    Add Support for Widths (#4)
    * Refactor to make sorting easier
    
    * Fixed sorting
    
    * Added font width sort
    
    * Missing async

--- code.ts
+++ code.ts
@@ -9,7 +9,6 @@ const fontWeightOrder = [
     'light',
     'normal',
     'regular',
-    '', // Include blank string to account for Regular Italic which is often just named Italic
     'medium',
     'semi bold',
     'semibold',
@@ -24,6 +23,17 @@ const fontWeightOrder = [
     'heavy',
     'ultra',
 ];
+const regularFontWeightIdx = 8;
+
+// List of common font widths ordered from widest to narrowest
+const fontWidthOrder = [
+    'wide',
+    'semiwide',
+    'narrow',
+    'condensed',
+    'compressed',
+];
+const regularFontWidthIdx = 1.5;
 
 // Event message from the plugin UI
 type PluginMessage = {
@@ -34,7 +44,19 @@ type PluginMessage = {
     fontFamily: string;
     textStyles: string[];
     textCases: TextCase[];
-    IsDescending: boolean;
+    isDescending: boolean;
+};
+
+// Font object that expands on Font to include parsed style information
+type FontParsed = {
+    fontName: FontName;
+    family: string;
+    style: string;
+    weight: string;
+    weightIdx: number;
+    width: string;
+    widthIdx: number;
+    isItalic: boolean;
 };
 
 // Default padding amount
@@ -64,42 +86,91 @@ async function loadFonts(fonts: FontName[]): Promise<void> {
 }
 
 /**
+ * Checks a string to see if it contains any one of a list of substrings
+ * @param String to search
+ * @param List of substrings to match on
+ * @returns Matched substring and its index in the substring list or `regular` if not found
+ */
+async function findMatchingSubstring(
+    str: string,
+    substrings: string[],
+    regularIdx: number,
+): Promise<[string, number]> {
+    // Sort substrings by length to ensure we match e.g. `extra bold` before `bold`
+    // while keeping track of original indexes to maintain the correct order
+    const sortedSubstrings = substrings
+        .map((substring, idx) => ({ substring, idx }))
+        .sort((a, b) => b.substring.length - a.substring.length);
+
+    for (const { substring, idx } of sortedSubstrings) {
+        if (str.includes(substring)) {
+            return [substring, idx];
+        }
+    }
+    return ['regular', regularIdx];
+}
+
+/**
+ * Parse a font into structured data with width, weight and italic information
+ * @param Font name
+ * @returns Font parsed
+ */
+async function parseFontName(font: Font): Promise<FontParsed> {
+    const style = font.fontName.style.toLowerCase();
+    const [weight, weightIdx] = await findMatchingSubstring(
+        style,
+        fontWeightOrder,
+        regularFontWeightIdx,
+    );
+    const [width, widthIdx] = await findMatchingSubstring(
+        style,
+        fontWidthOrder,
+        regularFontWidthIdx,
+    );
+    return {
+        fontName: font.fontName,
+        family: font.fontName.family,
+        style: style,
+        weight: weight,
+        weightIdx: weightIdx,
+        width: width,
+        widthIdx: widthIdx,
+        isItalic: style.includes('italic'),
+    };
+}
+
+/**
  * Creates a list of fonts required for the waterfall, ordered by font weight
  * @param Plugin message
  * @returns Returns font names once the fonts are fetched and ordered
  */
 async function createFontList(msg: PluginMessage): Promise<FontName[]> {
     const availableFonts = await figma.listAvailableFontsAsync();
+    const availableFontsParsed = await Promise.all(
+        availableFonts.map(parseFontName),
+    );
 
     const includeItalic = msg.textStyles.indexOf('italic') !== -1;
     const includeNonItalic = msg.textStyles.indexOf('non-italic') !== -1;
+    const m = msg.isDescending ? -1 : 1;
 
-    const filteredFonts = availableFonts.filter(
+    const filteredFonts = availableFontsParsed.filter(
         (font) =>
-            font.fontName.family === msg.fontFamily &&
-            ((font.fontName.style.toLowerCase().includes('italic') &&
-                includeItalic) ||
-                (!font.fontName.style.toLowerCase().includes('italic') &&
-                    includeNonItalic)),
+            font.family === msg.fontFamily &&
+            ((font.isItalic && includeItalic) || (!font.isItalic && includeNonItalic)),
     );
-
-    const m = msg.IsDescending ? -1 : 1;
 
     const sortedFonts = filteredFonts.sort((a, b) => {
         // Sort by italic/non-italic first
-        const aIsItalic = a.fontName.style.toLowerCase().includes('italic');
-        const bIsItalic = b.fontName.style.toLowerCase().includes('italic');
-        if (aIsItalic !== bIsItalic) {
-            return m * ((aIsItalic ? 1 : 0) - (bIsItalic ? 1 : 0));
+        if (a.isItalic !== b.isItalic) {
+            return m * ((a.isItalic ? 1 : 0) - (b.isItalic ? 1 : 0));
+        }
+        // Sort by width order
+        if (a.width !== b.width) {
+            return m * (a.widthIdx - b.widthIdx);
         }
         // Sort by weight order
-        const aWeightIdx = fontWeightOrder.indexOf(
-            a.fontName.style.toLowerCase().replace(/italic/i, '').trim(),
-        );
-        const bWeightIdx = fontWeightOrder.indexOf(
-            b.fontName.style.toLowerCase().replace(/italic/i, '').trim(),
-        );
-        return m * (aWeightIdx - bWeightIdx);
+        return m * (a.weightIdx - b.weightIdx);
     });
 
     return sortedFonts.map((font) => font.fontName);
